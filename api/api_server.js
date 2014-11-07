3a63d9e903d8751dea2ca44983b4dbe244c0daa0
Leonardo <leonardo.galani@iMac-de-Suporte.local>
2014-11-07 19:57:21 -0200

    client sessions

--- api/api_server.js
+++ api/api_server.js
@@ -1,11 +1,28 @@
 var restify = require('restify');
 var restifyMongoose = require('restify-mongoose');
 var mongoose = require('mongoose');
+var sessions = require("client-sessions");
 
 var server = restify.createServer({
     name: 'api.sherlock.v1',
     version: '0.1.0'
 });
+
+
+server.use(sessions({
+	cookieName: 'sherlock_auth_session',
+	secret: 'q8KY[82423BQ1;@D?!Ig}m{-41Y0}9DTxB90=N.7&BftM.28c}_9<W@RV5vl',
+	duration: 24 * 60 * 60 * 1000,
+	activeDuration: 1000 * 60 * 5
+	cookie: {
+		path: '/api',
+		maxAge: 60000,
+		ephemeral: false,
+		httpOnly: true,
+		secure: false
+	}
+}));
+
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -14,9 +31,9 @@ server.use(restify.bodyParser());
 // Create a simple mongoose model 'Note'
 var UserSchema = new mongoose.Schema({
 	user : { type : String, required : true },
-    email : { type : String, required : true },
-    avatar : { type : String },
-    projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }]
+	email : { type : String, required : true },
+	avatar : { type : String },
+	projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }]
 });
 
 var ProjectSchema = new mongoose.Schema({
@@ -48,7 +65,6 @@ restifyMongoose(models.Cenarios).serve('/api/cenario', server);
 restifyMongoose(models.Cases).serve('/api/case', server);
 
 
-
 server.listen(3000, function () {
-    console.log('%s listening at %s', server.name, server.url);
+	console.log('%s listening at %s', server.name, server.url);
 });
