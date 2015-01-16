650be91c59197fd71aab234615015ad66be9eda9
Leonardo <leonardo.galani@iMac-de-Suporte.local>
2015-01-16 17:40:49 -0200

    Front End Folders

---
+++ public/sherlock_front.js
@@ -0,0 +1,1 @@
+Sherlock = Ember.Application.create();
---
+++ public/static/libs/ember-handlebars-loader.js
@@ -0,0 +1,16 @@
+//v0.0.1
+//https://github.com/michaelrkn/ember-handlebars-loader
+var EmberHandlebarsLoader = {
+  loadTemplates: function(templateNames) {
+    templateNames.forEach(function(name) {
+      $.ajax({
+        url: "templates/" + name + ".hbs",
+        async: false,
+        success: function(template) {
+          var compiledTemplate = Ember.Handlebars.precompile(template);
+          Ember.TEMPLATES[name] = Ember.Handlebars.template(compiledTemplate);
+        }
+      });
+    });
+  }
+};
